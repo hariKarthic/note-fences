804e90fc15b82f6cca52d894963b6ef992aa75f5
= <=>
2018-09-16 00:21:19 +0530

    Added sentry logging service

--- src/index.js
+++ src/index.js
@@ -3,5 +3,18 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
-registerServiceWorker();
+const initApp = () => {
+  ReactDOM.render(<App />, document.getElementById("root"));
+  registerServiceWorker();
+};
+
+if (window.Raven) {
+  // configure and install sentry
+  window.Raven.config(
+    "https://a46dee972cca4fd2b517fe1af1f1b671@sentry.io/1281689"
+  ).install();
+  window.Raven.context(initApp);
+  console.info("Sentry configuration done!");
+} else {
+  initApp();
+}
